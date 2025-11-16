b380c50de4dec4d73feeec171f57f953a3aadb5f
potlapatiDinesh <140254669+potlapatiDinesh@users.noreply.github.com>
2025-11-16 21:47:53 +0530

    Add AWS route table resource configuration

---
+++ routetable.tf
@@ -0,0 +1,10 @@
+resource "aws_route_table" "my_route" {
+  tags = {
+    Name = "dinu-route"
+    }
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_blocks = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+    }
+  
